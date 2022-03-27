ce9be9be014a8540064ab38b59a1ec77b0b0918e
getinthere <getinthere@naver.com>
2022-03-27 16:34:25 +0900

    info error warn 수정

--- src/main/java/com/metacoding/v1/web/HelloController.java
+++ src/main/java/com/metacoding/v1/web/HelloController.java
@@ -16,9 +16,9 @@ public class HelloController {
         if(number == 1){ // info 로그
             log.info("/aws/v1 이 호출되었어요. info 로그 #####################################");
         }else if(number == -1){ // error 로그
-            log.info("/aws/v1 이 호출되었어요. error 로그 #####################################");
+            log.error("/aws/v1 이 호출되었어요. error 로그 #####################################");
         }else if(number == 0){ // warn 로그
-            log.info("/aws/v1 이 호출되었어요. warn 로그 #####################################");
+            log.warn("/aws/v1 이 호출되었어요. warn 로그 #####################################");
         }
         
         return "<h1>aws v1</h1>";
